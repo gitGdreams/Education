08870a646e7be691fa2d923936ef82ef7794359c
gitGdreams <161871457+gitGdreams@users.noreply.github.com>
2024-04-22 06:58:02 -0700

    Update MAC-Changer.py

--- PersonalTime/Cyber/Orielly/LearnPythonandEthicalHackingfromSratch/MAC-Changer.py
+++ PersonalTime/Cyber/Orielly/LearnPythonandEthicalHackingfromSratch/MAC-Changer.py
@@ -1,13 +1,6 @@
 import subprocess
 
-def change_mac(interface, new_mac):
-    subprocess.call(["sudo", "ifconfig", interface, "down"])
-    subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
-    subprocess.call(["sudo", "ifconfig", interface, "up"])
-
-# Example usage:
-# change_mac("eth0", "00:11:22:33:44:55")
 
 subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
+subprocess.call("ifconfig wlan0 hw ether [macaddr]", shell=True)
 subprocess.call("ifconfig wlan0 up", shell=True)
