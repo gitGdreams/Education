be3e7ee21b776e4b4ce33a327ee5cc480a38551d
gitGdreams <161871457+gitGdreams@users.noreply.github.com>
2024-04-22 07:16:15 -0700

    Update MAC-Changer.py

--- PersonalTime/Cyber/Orielly/LearnPythonandEthicalHackingfromSratch/MAC-Changer.py
+++ PersonalTime/Cyber/Orielly/LearnPythonandEthicalHackingfromSratch/MAC-Changer.py
@@ -7,7 +7,10 @@ subprocess.call(f"ifconfig {interface} down", shell=True)
 subprocess.call(f"ifconfig {interface} hw ether {macaddr}", shell=True)
 subprocess.call(f"ifconfig {interface} up", shell=True)
 
+print(f"MAC address of {interface} has been changed to {macaddr}.")
 
-
-
-
+verify_after = input("Verify interface after MAC address change (y/n)? ")
+if verify_after.lower() == 'y':
+    subprocess.call(f"ifconfig {interface}")
+else:
+    print("Verification skipped.")
