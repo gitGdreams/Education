260f5c6535823ddb375966e43135f83142b1620f
gitGdreams <161871457+gitGdreams@users.noreply.github.com>
2024-04-22 07:02:50 -0700

    Update MAC-Changer.py

--- PersonalTime/Cyber/Orielly/LearnPythonandEthicalHackingfromSratch/MAC-Changer.py
+++ PersonalTime/Cyber/Orielly/LearnPythonandEthicalHackingfromSratch/MAC-Changer.py
@@ -1,10 +1,10 @@
 import subprocess
 
 interface = input("What interface are we replacing?")
-mac
+macaddr = input("What will be the new MAC address?")
 
 subprocess.call(f"ifconfig {interface} down", shell=True)
-subprocess.call(f"ifconfig {interface} hw ether [macaddr]", shell=True)
+subprocess.call(f"ifconfig {interface} hw ether {macaddr}", shell=True)
 subprocess.call(f"ifconfig {interface} up", shell=True)
 
 
