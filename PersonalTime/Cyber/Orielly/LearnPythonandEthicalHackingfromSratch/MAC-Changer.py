a12cdd000cc4e0eb17ba03b783413fed36448da1
gitGdreams <161871457+gitGdreams@users.noreply.github.com>
2024-04-22 06:56:28 -0700

    Update MAC-Changer.py

--- PersonalTime/Cyber/Orielly/LearnPythonandEthicalHackingfromSratch/MAC-Changer.py
+++ PersonalTime/Cyber/Orielly/LearnPythonandEthicalHackingfromSratch/MAC-Changer.py
@@ -8,3 +8,6 @@ def change_mac(interface, new_mac):
 # Example usage:
 # change_mac("eth0", "00:11:22:33:44:55")
 
+subprocess.call("ifconfig wlan0 down", shell=True)
+subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
+subprocess.call("ifconfig wlan0 down", shell=True)
